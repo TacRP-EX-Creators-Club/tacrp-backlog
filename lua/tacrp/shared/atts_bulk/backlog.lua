44b56251542febab3e8a569f1e789df8da252953
TheOnly8Z <888888zombies@gmail.com>
2026-05-28 22:47:28 -0400

    Moved Grizzly to main pack

--- lua/tacrp/shared/atts_bulk/backlog.lua
+++ lua/tacrp/shared/atts_bulk/backlog.lua
@@ -1,27 +1,3 @@
 -- backlog.lua
 
 local ATT = {}
-
-------------------------------
--- muzz_comp_grizzly
-------------------------------
-ATT = {}
-
-ATT.PrintName = "att.muzz_pistol_comp.name"
-ATT.Icon = Material("entities/tacrp_att_muzz_pistol_comp.png", "mips smooth")
-ATT.Description = "att.muzz_pistol_comp.desc"
-ATT.Pros = {"stat.recoil", "stat.spread", "stat.range_min"}
-ATT.Cons = {"stat.rpm"}
-
-ATT.Category = "comp_grizzly"
-ATT.InvAtt = "muzz_pistol_comp"
-ATT.SortOrder = 1
-
-ATT.Mult_RecoilKick = 0.5
-ATT.Mult_RPM = 0.9
-ATT.Mult_Spread = 0.75
-ATT.Mult_Range_Min = 1.5
-
-ATT.InstalledElements = {"pistol_comp"}
-
-TacRP.LoadAtt(ATT, "muzz_comp_grizzly")
